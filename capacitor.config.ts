daf2ac9f5efb558955d386695d34d5915c969234
Bayu <bayusarifudin@gmail.com>
2026-02-24 19:28:57 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'unictive-test',
+  webDir: 'www'
+};
+
+export default config;
